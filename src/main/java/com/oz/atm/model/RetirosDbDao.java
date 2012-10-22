476c738f3461b9c325ebf7aa606cd8c3fd354192
Alberto <jaehoo@gmail.com>
2012-10-22 18:09:00 -0500

    Added project libraries, and empty structure of classes

---
+++ src/main/java/com/oz/atm/model/RetirosDbDao.java
@@ -0,0 +1,16 @@
+package com.oz.atm.model;
+
+/**
+ * Created by Orbital Zero.
+ * Date: 22/10/12
+ * Time: 11:28 AM
+ * Author: Lic. José Alberto Sánchez González
+ * Twitter: @jaehoox
+ * mail: <a href="mailto:jaehoo@gmail.com">jaehoo@gmail.com</a>
+ */
+public class RetirosDbDao implements Retiros{
+    @Override
+    public void retiro(Double importe) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
